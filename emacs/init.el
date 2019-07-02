86eef0de9a71d75e9107a9e2be8f44481901adf1
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2019-07-02 23:47:33 +0200

    Add minor whitespace-mode for visualisation

--- emacs/init.el
+++ emacs/init.el
@@ -1,12 +1,33 @@
-;; prevent screen spliting after startup
+; prevent screen spliting after startup
 (setq inhibit-startup-screen t)
+; maximize emacs frame on start-up
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; create backup files in /tmp directory
-(setq backup-directory-alist '(("." . "/tmp/emacs"))
-  backup-by-copying t    ; Don't delink hardlinks
-  version-control t      ; Use version numbers on backups
-  delete-old-versions t  ; Automatically delete excess backups
-  kept-new-versions 20   ; how many of the newest versions to keep
-  kept-old-versions 5    ; and how many of the old
-  )
-;; fjksdfsd
+; load dracula theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/dracula-theme.el")
+(load-theme 'dracula t)
+
+; enable whitespace-mode
+(require 'whitespace)
+(global-whitespace-mode 1)
+; whitespace display style
+(setq whitespace-style '(
+                         face ; enable visualisation with faces (highlights)
+                         ; characters visualised with faces
+                         trailing
+                         tabs
+                         lines-tail
+                         newline
+                         empty ; lines at the beginning and end of the buffer
+                         indentation
+                         big-inden
+                         ; characters visualised with marks
+                         space-mark
+                         ))
+
+; stop creating backup~ files
+(setq make-backup-files nil)
+; stop creating #autosave# files
+(setq auto-save-default nil)
+; never insert tabs
+(setq-default indent-tabs-mode nil)
