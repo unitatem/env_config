009e221ed6e44ec4e433b947f957dca408268317
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2022-04-09 16:29:59 +0200

    Clarify usage of keys

--- shell/zsh/setup.bash
+++ shell/zsh/setup.bash
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 set -e
-# SHELL_SUDO
-# SHELL_PACKAGE_MANAGER
-# SHELL_INSTALL_TERMINAL
-# SHELL_INSTALL_COLORLS
-# SHELL_CREATE_SYMLINK
+YES="Y"
 
-if [ -z ${SHELL_INSTALL_SUDO+x} ]; then SHELL_INSTALL_SUDO=sudo; fi
-if [ -z ${SHELL_PACKAGE_MANAGER+x} ]; then SHELL_PACKAGE_MANAGER=apt-get; fi
-
+if [ ! "$SHELL_I_HAVE_READ" = "$YES" ]; then
+	echo -e '
+	# Environment variables controlling installations process.
+	# Set (or not set) before running setup script.
+	# Key:                    Value Ubuntu  Value Centos  Comment
+	# SHELL_I_HAVE_READ       Y             Y             Must be set to Y
+	# SHELL_SUDO              sudo          ?
+	# SHELL_PACKAGE_MANAGER   apt-get       yum
+	# SHELL_INSTALL_TERMINAL  Y/N           Y/N
+	# SHELL_INSTALL_COLORLS   Y/N           Y/N
+	# SHELL_CREATE_SYMLINK    Y/N           Y/N
+	'
+	exit -1
+fi
 
 # Resources
 # https://ivanaugustobd.medium.com/your-terminal-can-be-much-much-more-productive-5256424658e8
@@ -17,7 +24,7 @@ if [ -z ${SHELL_PACKAGE_MANAGER+x} ]; then SHELL_PACKAGE_MANAGER=apt-get; fi
 $SHELL_SUDO $SHELL_PACKAGE_MANAGER -y update
 
 # Install terminal
-if [ ! -z ${SHELL_INSTALL_TERMINAL+x} ]; then
+if [ "$SHELL_INSTALL_TERMINAL" = "$YES" ]; then
 	$SHELL_SUDO $SHELL_PACKAGE_MANAGER -y install tilix
 fi
 
@@ -37,7 +44,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # use CTRL+T to search for files
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/projects/fzf && ~/projects/fzf/install
-if [ ! -z ${SHELL_INSTALL_COLORLS+x} ]; then
+if [ "$SHELL_INSTALL_COLORLS" = "$YES" ]; then
 	$SHELL_SUDO gem install colorls
 fi
 
@@ -53,13 +60,14 @@ cp /tmp/Hack/* .local/share/fonts/.
 fc-cache -f -v
 
 
-if [ -z ${SHELL_CREATE_SYMLINK+x} ]; then
-	echo "Symlink zshrc file in the repository to the ~/.zshrc"
-	echo "Symlink p10k.zsh file in the repository to the ~/.p10k.zsh"
-else
+if [ "$SHELL_CREATE_SYMLINK" = "$YES" ]; then
 	rm -f $HOME/.zshrc
 	ln -s $PWD/zshrc $HOME/.zshrc
 	ln -s $PWD/p10k.zsh $HOME/.p10k.zsh
+else
+	echo "Symlink zshrc file in the repository to the ~/.zshrc"
+	echo "Symlink p10k.zsh file in the repository to the ~/.p10k.zsh"
 fi
 
 echo "Run zsh :)"
+
